b766c743f68250dd052ac2f7b7b6f8740ce90a9c
simo <simone.riolfo07@gmail.com>
2026-02-26 18:28:26 +0100

    modified by PEP 8 guide

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import json
 
 FILENAME = 'tasks.json'
 
+
 def load_tasks():
     try:
-        with (open(FILENAME, 'r') as file):
+        with open(FILENAME, 'r') as file:
             return json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         return {}
 
+
 def save_tasks(tasks):
     with open(FILENAME, 'w') as file:
         json.dump(tasks, file, indent=4)
+
 
 def show_tasks(tasks):
     if not tasks:
@@ -21,11 +24,13 @@ def show_tasks(tasks):
 
     print('These are your tasks:')
     for i, (key, value) in enumerate(tasks.items(), 1):
-        print(f'{i}. {key} {value}')
+        print(f'{i}. {value} {key}')
     return True
 
-def complete_task():
+
+def complete_task(tasks):
     pass
+
 
 def main():
     print('\nHi! This is a to-do list application.')
@@ -33,7 +38,7 @@ def main():
     tasks = load_tasks()
 
     while True:
-        print('\n=========================== What do you want to do? ===========================')
+        print('\n' + '=' * 27 + ' What do you want to do? ' + '=' * 27)
         print('1.   Add a task')
         print('2.   Remove a task')
         print('3.   View your tasks')
@@ -44,7 +49,7 @@ def main():
 
         match choice:
             case '1':
-                print('\n=========================== Add a task ===========================')
+                print('\n' + '=' * 27 + ' Add a task ' + '=' * 27)
                 task = input('Write here your task: ').strip()
                 if task:
                     tasks.update({task: '[ ]'})
@@ -54,32 +59,37 @@ def main():
                     print('Task cannot be empty.')
 
             case '2':
-                print('\n=========================== Remove a task ===========================')
+                print('\n' + '=' * 27 + ' Remove a task ' + '=' * 27)
                 if show_tasks(tasks):
-                    task = input('Remove task: ')
-                    removed = tasks.pop(task)
-                    save_tasks(tasks)
-                    print(f'Task "{removed}" removed successfully.')
+                    task = input('Remove task (type exact name): ').strip()
+                    if task in tasks:
+                        tasks.pop(task)
+                        save_tasks(tasks)
+                        print(f'Task "{task}" removed successfully.')
+                    else:
+                        print(f'Task "{task}" not found.')
 
             case '3':
-                print('\n=========================== View your tasks ===========================')
+                print('\n' + '=' * 27 + ' View your tasks ' + '=' * 27)
                 show_tasks(tasks)
 
             case '4':
-                complete_task()
+                print('\n' + '=' * 27 + ' Complete a task ' + '=' * 27)
+                complete_task(tasks)
 
             case '5':
-                print('\n=========================== Exit ===========================')
-                end = input('Are tou sure? (y/n) ').strip().lower()
+                print('\n' + '=' * 27 + ' Exit ' + '=' * 27)
+                end = input('Are you sure? (y/n) ').strip().lower()
                 if end == 'y':
                     break
                 elif end != 'n':
-                    print('Invalid input')
+                    print('Invalid input.')
 
             case _:
-                print('\nInvalid choice. Please enter a number between 1 and 4.')
+                print('\nInvalid choice. Please enter a number between 1 and 5.')
 
     print('\nThanks for using my application!')
+
 
 if __name__ == '__main__':
     main()
